f7d3705d25b8ced7da2ed64a4b17795bad1cd512
SimoneCappadona <simocappa95@gmail.com>
2024-01-19 15:15:00 +0100

    fix grid

--- js/function.js
+++ js/function.js
@@ -1,11 +1,22 @@
-function select(difficult, difInner) {
-  difInner.innerHTML = "";
-  for (let i = 1; i <= difficult; i++) {
-    const div = document.createElement("div");
-    div.addEventListener("click", function () {
-      div.className = "square";
-    });
-    div.append(i);
-    inner.append(div);
+function pippo(difficult, grid) {
+  grid.innerHTML = "";
+  let difficultSelection ; 
+  if (difficult.value == "Easy") {
+    difficultSelection = 100;
+  } else if (difficult.value == "Medium") {
+    difficultSelection = 81;
+  } else if (difficult.value == "Hard"){
+    difficultSelection = 49;
+  }else  {
   }
+  for (let i = 1; i <= difficultSelection; i++) {
+    const cella = document.createElement("div");
+    cella.addEventListener("click", function () {
+      cella.className = "square";
+      cella.innerText = i;
+    });
+    grid.append(cella);
+  }
+  
 }
+
